e1dd56b1fced93325866f1a77fddcd57dc61d9b8
Jan Narozny <rzachulec@gmail.com>
2025-02-11 15:25:02 +0100

    Initial Commit

---
+++ Pace/PaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaceApp.swift
+//  Pace
+//
+//  Created by jan on 11/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct PaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PaceTests/PaceTests.swift
@@ -0,0 +1,16 @@
+//
+//  PaceTests.swift
+//  PaceTests
+//
+//  Created by jan on 11/02/2025.
+//
+
+import Testing
+
+struct PaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
